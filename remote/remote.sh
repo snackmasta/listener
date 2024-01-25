97fd4e81380d4cd63dd0661df26d80f6268696d4
currone <94299594+snackmasta@users.noreply.github.com>
2024-01-25 20:12:54 +0700

    Update remote.sh

--- remote/remote.sh
+++ remote/remote.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-declare -a connections=()  # Array to store connection details
+declare -a connections=()
 stty_state=$(stty -g)
 stty raw -echo
 
 while true; do
     # Accept incoming connection and store details in array
-    connection_info=$( (stty size; cat) | nc -lvnp 2002 )
+    connection_info=$( (stty size; timeout 1 cat) | nc -lvnp 2002 )
     connections+=("$connection_info")
 
     # Display menu of available connections
