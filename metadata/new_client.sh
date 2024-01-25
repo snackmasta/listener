89da7dce25ede812f9b642419abdf1487ca2b29b
currone <agungrambujana@gmail.com>
2024-01-25 14:30:24 +0700

    json

--- metadata/new_client.sh
+++ metadata/new_client.sh
@@ -1,1 +1,8 @@
-jq '.clients += [{"id": "new_client", "state": 0.9}]' data_old.json > data.json; cat data.json > data_old.json
+#!/bin/bash
+
+num= 
+
+jq '.clients += {"client_1":{"num": '$num' "id": "new_client", "state": 0.9}}' data_old.json > data.json
+
+sleep 1
+cp data.json data_old.json
