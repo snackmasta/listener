03007dcba726d379c00194cf2a46e90365cf62d0
currone <agungrambujana@gmail.com>
2024-01-25 08:17:15 +0700

    data

--- update_address.sh
+++ update_address.sh
@@ -7,12 +7,16 @@ new_ip=$(host 0.tcp.ap.ngrok.io | grep -oP 'has address \K[^ ]+')
 new_port=$(curl localhost:4040/api/tunnels | jq '.tunnels[0].public_url'| awk -F':' '{print $3}'|tr -d '"')
 
 # Create a JSON string with the new IP and port
-json_data="{\"ip_address\":\"$new_ip\",\"port\":\"$new_port\",\"id\":\"$id\"}"
+#json_data="{\"ip_address\":\"$new_ip\",\"port\":\"$new_port\",\"id\":\"$id\"}"
 
-# Echo the JSON string to data.json
-echo "$json_data" > data.json
+#jquery new ip and port 
+jq '.website == [{"ip": "$new_ip", "port": "$new_port"}]' ./metadata/data_old.json > ./metadata/data.json
+sleep 1 
 
-git add data.json
+#Echo the JSON string to data.json
+cat ./metadata/data.json > ./metadata/data_old.json
+
+git add .
 git commit -m data 
 git push
 
