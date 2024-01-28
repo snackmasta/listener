e29384c1df2d109a3fe6f961d8f1cf49217b1dc7
currone <94299594+snackmasta@users.noreply.github.com>
2024-01-28 22:08:39 +0700

    update.sh

--- updata.sh
+++ updata.sh
@@ -6,13 +6,13 @@ new_ip=$(host 0.tcp.ap.ngrok.io | grep -oP 'has address \K[^ ]+')
 # Read the new port
 new_port=$(curl localhost:4040/api/tunnels | jq '.tunnels[0].public_url' | awk -F':' '{print $3}' | tr -d '"')
 
-# jq to update new IP and port
-old_data=./metadata/data_old.json
-new_data=./metadata/data.json
+# # jq to update new IP and port
+# old_data=./metadata/data_old.json
+# new_data=./metadata/data.json
 
-jq '.website.ip="'$new_ip'" | .website.port='$new_port'' "$old_data" > "$new_data"
-sleep 1
-cp "$new_data" "$old_data"
+# jq '.website.ip="'$new_ip'" | .website.port='$new_port'' "$old_data" > "$new_data"
+# sleep 1
+# cp "$new_data" "$old_data"
 
 curl -X PUT -d '{"port": '$new_port', "ip": "'$new_ip'"}' 'https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app/address.json' > /dev/null
 
