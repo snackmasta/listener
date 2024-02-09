abd31f87ccb3e64b374894f9ad3467596d7963db
currone <94299594+snackmasta@users.noreply.github.com>
2024-02-09 22:56:43 +0700

    basic Online View

--- remote/mac.sh
+++ remote/mac.sh
@@ -1,17 +1,20 @@
-json_data='{
-  "00-15-5D-38-01-09": {
-    "LastSeen": "20:32:14 Thursday, 08 February 2024"
-  },
-  "3C-A0-67-66-D6-05": {
-    "LastSeen": "07:36:02 Wednesday, 07 February 2024"
-  },
-  "AC-12-03-FC-5C-EC": {
-    "LastSeen": "11:32:12 Friday, 09 February 2024"
-  },
-  "F4-6A-DD-6A-CF-4F": {
-    "LastSeen": "08:02:59 Wednesday, 07 February 2024"
-  }
-}'
+url="https://curronebox-default-rtdb.asia-southeast1.firebasedatabase.app"
+json_data=$(curl -s "$url/Online.json")
+count=$(echo "$json_data" | jq 'length')
+# echo "Number of key-value pairs: $count"
 
-mac_address=$(echo "$json_data" | jq -r 'keys[1]')
-echo "$mac_address"
+# Scan 0 diff Client time
+for (( i = 0; i <= $count-1; i++ )); do
+  # echo $i
+  # echo "$json_data" | jq -r 'keys['$i']'
+  current_time=($(date "+%H") $(date "+%M"))
+  mac_address=$(echo "$json_data" | jq -r 'keys['$i']')
+  time_value=$(echo "$json_data" | jq -r '.["'$mac_address'"].LastSeen' | cut -d ' ' -f1)
+  client_hours=$(echo "$time_value" | cut -d':' -f1 | sed 's/^0*//')
+  client_minutes=$(echo "$time_value" | cut -d':' -f2 | sed 's/^0*//')
+  compare_time=$(( (client_hours - ${current_time[0]}) * 60 + client_minutes - ${current_time[1]} ))
+  # echo $compare_time
+  if [ "$compare_time" -ge -1 ] && [ "$compare_time" -le 1 ]; then
+    echo "$mac_address is Online"
+  fi
+done
