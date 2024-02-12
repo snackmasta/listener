b97c5b85459852023c888611858756c7bb054ff6
currone <agungrambujana@gmail.com>
2024-02-12 21:04:36 +0700

    exitPrompt

--- remote/stty.sh
+++ remote/stty.sh
@@ -2,7 +2,12 @@
 stty_state=$(stty -g); 
 while :
 do
-    stty raw -echo; 
+    stty raw -echo time 0; 
     (stty size; cat) | nc -lvnp 2002; 
     stty "$stty_state";
+    echo "Listening stopped. Press 'r' to restart or 'e' to exit."
+    read -n 1 choice
+    if [ "$choice" = "e" ]; then
+        exit 0
+    fi
 done
